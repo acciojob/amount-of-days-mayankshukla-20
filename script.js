864131e07d5f675f90422fac69045fe5f386239f
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-14 17:00:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(n){
+	if(n % 4 === 0){
+		if(n % 100 === 0 && n % 400 === 0){
+			return 366;
+		}
+		return 365;
+	}
+	else{
+		return 365;
+	}
+}
